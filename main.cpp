709ac531b1f75f1d2dacfb8f44293de8d393aa47
Kirill Stepankov <fpm.stepanko@bsu.by>
2021-11-03 22:50:22 +0300

    Add assert to numerator<+denominator

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 int main() {
     NormalFraction fraction3(3,4);
     NormalFraction fraction1(fraction3);
-    fraction1.Addition({-8,3});
+    fraction1.Addition({-2,3});
     fraction1.PrintFraction();
-    fraction1.Multiplication({6,5});
+    fraction1.Multiplication({4,5});
     fraction1.PrintFraction();
     return 0;
 }
